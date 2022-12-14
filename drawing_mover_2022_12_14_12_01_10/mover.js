45abe94f3459b27826d4df37f98fb7edb0e022bc
yena0910 <115522688+yena0910@users.noreply.github.com>
2022-12-15 02:16:07 +0900

    부드러운 움직임
    noise를 이용한 부드러운 움직임을 만들어보았다.

--- drawing_mover_2022_12_14_12_01_10/mover.js
+++ drawing_mover_2022_12_14_12_01_10/mover.js
@@ -1,9 +1,10 @@
 class Mover {
     constructor(x, y, m) {
       this.pos = createVector(x, y);
-      this.vel = createVector(random(-5, 5), random(-5, 5));
+    //  this.vel = createVector();
       this.acc = createVector();
       this.m = m; 
+      this.t = 0;
     }
     
     applyForce(aForce) {
@@ -12,11 +13,28 @@ class Mover {
     }
     
     update() {
-      this.vel = createVector(random(-1, 1), random(-1, 1));
+      
+      this.t = this.t + 0.1;
+      let speedx = 5 * (noise(this.t) - 0.4);
+      let speedy = 5 * (noise(-this.t + 10) - 0.4);
+
+      this.vel = createVector(speedx, speedy * -1);
       this.vel.add(this.acc);
       this.pos.add(this.vel);
       this.acc.set(0, 0);
+
+
+      
+
+    //   if(speed>0.2) {
+    //     speed = speed * -1 ;
+    //   }
+
+    //   if(speed>-0.2 && speed<0) {
+    //     speed = speed * -1 ;
+    //   }
     }
+
     
     show() {
       fill(0);
--- drawing_mover_2022_12_14_12_01_10/sketch.js
+++ drawing_mover_2022_12_14_12_01_10/sketch.js
@@ -3,15 +3,13 @@ let canvas2;
 let ball;
 
 
+
 function setup() {
-  createCanvas(300, 300);
-  canvas2 = createGraphics(300, 300);
+  createCanvas(800, 800);
+  canvas2 = createGraphics(800, 800);
   ball = new Mover(width/2, height/2, 5);
+  
 }
-
-
-
-
 
 
 
@@ -38,37 +36,3 @@ function draw() {
 
 
 
-
-
-
-class Mover {
-  constructor(x, y, m) {
-    this.pos = createVector(x, y);
-    this.vel = createVector(random(-5, 5), random(-5, 5));
-    this.acc = createVector();
-    this.m = m; 
-  }
-  
-  applyForce(aForce) {
-    let f = p5.Vector.div(aForce, this.m);
-    this.acc.add(f);
-  }
-  
-  update() {
-    this.vel = createVector(random(-1, 1), random(-1, 1));
-    this.vel.add(this.acc);
-    this.pos.add(this.vel);
-    this.acc.set(0, 0);
-  }
-  
-  show() {
-    fill(0);
-    circle(this.pos.x, this.pos.y, this.m);
-  }
-  
-  show2(aCanvas) {
-    aCanvas.noStroke();
-    aCanvas.fill(155);
-    aCanvas.circle(this.pos.x, this.pos.y, this.m);
-  }
-}

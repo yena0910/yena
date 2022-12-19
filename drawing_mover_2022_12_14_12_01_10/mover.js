839c18711d1c6b1d05dc8b8e06427bdbeb17f40e
yena0910 <yenayena1217@naver.com>
2022-12-19 18:42:38 +0900

    경로에 뻗는 가지를 만들었다.
    코드의 오류가 계속 생겨 오류의 원인을 알고 싶어 vs.code에서하던 작업을 p5.js에서 옮겨서 하였다. 그걸 다시 옮긴 코드여서 변동이 많이 보일것이다.

--- drawing_mover_2022_12_14_12_01_10/mover.js
+++ drawing_mover_2022_12_14_12_01_10/mover.js
@@ -1,85 +1,95 @@
 class Mover {
-    constructor(x, y, m) {
-      this.pos = createVector(x, y);
+  constructor(x, y, m) {
+    this.pos = createVector(x, y);
     //  this.vel = createVector();
-      this.acc = createVector();
-      this.m = m; 
-      this.t = 0;
-      this.wallx = 1;
-      this.wally = 1;
-      this.path = [];
-      this.length = 150;
-    }
-    
-    applyForce(aForce) {
-      let f = p5.Vector.div(aForce, this.m);
-      this.acc.add(f);
-    }
-    
-    update() {
-      
-      this.t = this.t + 0.01;
-      let speedx = 20 * (noise(this.t) - 0.5) * this.wallx;
-      let speedy = 20 * (noise(this.t - 10) - 0.5) * this.wally;
+    this.acc = createVector();
+    this.m = m;
+    this.t = 0;
+    this.wallx = 1;
+    this.wally = 1;
+    this.path = [];
+    this.length = 150;
+    this.theta;
+    this.seed = [];
+  }
 
-      this.vel = createVector(speedx, speedy);
-      this.vel.add(this.acc);
-      this.pos.add(this.vel);
-      this.acc.set(0, 0);
-      
-      this.path.push(this.pos.copy());
-      if (this.path.length > this.length) {
-        this.path.splice(0, 1);
+  applyForce(aForce) {
+    let f = p5.Vector.div(aForce, this.m);
+    this.acc.add(f);
+  }
+
+  update() {
+    this.t = this.t + 0.01;
+    let speedx = 20 * (noise(this.t) - 0.5) * this.wallx;
+    let speedy = 20 * (noise(this.t - 10) - 0.5) * this.wally;
+
+    this.vel = createVector(speedx, speedy);
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.set(0, 0);
+
+    this.path.push(this.pos.copy());
+    if (this.path.length > this.length) {
+      this.path.splice(0, 1);
+    }
+
+    this.seed.push(this.pos.copy());
+    if (this.seed.length > 30) {
+      this.seed.splice(0, 30);
+    }
+  }
+
+  edge() {
+    if (this.pos.y - this.m / 2 < 0) {
+      this.wally = this.wally * -1;
+      this.pos.y = 0 + this.m / 2;
+    }
+    if (this.pos.x - this.m / 2 < 0) {
+      this.wallx = this.wallx * -1;
+      this.pos.x = 0 + this.m / 2;
+    }
+    if (this.pos.y + this.m / 2 > height) {
+      this.wally = this.wally * -1;
+      this.pos.y = height - this.m / 2;
+    }
+    if (this.pos.x + this.m / 2 > width) {
+      this.wallx = this.wallx * -1;
+      this.pos.x = width - this.m / 2;
+    }
+  }
+
+  show() {
+    fill(0);
+    circle(this.pos.x, this.pos.y, this.m);
+
+    for (let i = 0; i < this.path.length - 2; i++) {
+      line(
+        this.path[i].x,
+        this.path[i].y,
+        this.path[i + 1].x,
+        this.path[i + 1].y
+      );
+    }
+    
+    
+        this.h = 20;
+
+    if (this.seed.length > 20) {
+      push();
+      translate(this.seed[15].x, this.seed[15].y);
+      line(0, 0, 40, 40);
+      translate(40, 40);
+
+      this.h *= 0.66;
+
+      if (this.h > 0.3) {
+        rotate(radians(30));
+        line(0, 0, 0, -this.h);
+        translate(0, -this.h);
+
+        pop();
       }
     }
-
-
-
-    edge() {
-        if(this.pos.y - this.m/2 < 0) {
-            this.wally = this.wally * -1;
-            this.pos.y = 0 + this.m/2;
-        }
-        if(this.pos.x - this.m/2 < 0) {
-            this.wallx = this.wallx * -1;
-            this.pos.x = 0 + this.m/2;
-        }
-        if(this.pos.y + this.m/2 > height) {
-            this.wally = this.wally * -1;
-            this.pos.y = height - this.m/2;
-        }
-        if(this.pos.x + this.m/2 > width) {
-            this.wallx = this.wallx * -1;
-            this.pos.x = width - this.m/2;
-        }
-
-    }
-
-
-
-    
-    show() {
-      fill(0);
-      circle(this.pos.x, this.pos.y, this.m);
-     
-
-      for (let i = 0; i < this.path.length - 2; i++)  { 
-          line(this.path[i].x, this.path[i].y, this.path[i + 1].x, this.path[i + 1].y);
-         // circle(this.path[i].x, this.path[i].y, this.m); 
-        }         
-       } 
-
-
-
-  seed() {
-     
-     }
-    
-    
-   // show2(aCanvas) {
-   //   aCanvas.noStroke();
-   //   aCanvas.fill(155);
-   //   aCanvas.circle(this.pos.x, this.pos.y, this.m);
-   // }
   }
-  
+
+ }
--- drawing_mover_2022_12_14_12_01_10/sketch.js
+++ drawing_mover_2022_12_14_12_01_10/sketch.js
@@ -1,32 +1,22 @@
-
 let canvas2;
 let ball;
-
-
 
 function setup() {
   createCanvas(800, 800);
   canvas2 = createGraphics(800, 800);
-  ball = new Mover(width/2, height/2, 3);
-  
+  ball = new Mover(width / 2, height / 2, 3);
 }
-
-
-
-
 
 function draw() {
   background(220);
   image(canvas2, 0, 0);
   let mousePos = createVector(mouseX, mouseY);
 
-  
-    
-   ball.update();
-   ball.show();
-  // ball.show2(canvas2);
-   ball.edge();
-  }
+  ball.update();
+  ball.show();
+  ball.edge();
+
+}
 
 
 

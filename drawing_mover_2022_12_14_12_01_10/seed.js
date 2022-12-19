bc824211cadf623ec1968513fc646af7c38c2370
yena0910 <yenayena1217@naver.com>
2022-12-20 05:38:45 +0900

    색상 추가
    파티클에 색상을 추가했다

--- drawing_mover_2022_12_14_12_01_10/seed.js
+++ drawing_mover_2022_12_14_12_01_10/seed.js
@@ -1,36 +1,36 @@
-
 class Branch {
-  constructor(x, y) {
-    this.pos = createVector(x, y);
-    this.ell = 0.0;
-    this.vel = 20; 
-    this.acc = 0.2;
-    this.core = createVector(random(-2, 2),random(-2, 2));
-    this.lifespan = 255;
-  }
-  
-     addForce(aForce) {
-     this.core.add(aForce);
-   }
-  
-  update() {
-     this.ell += this.vel;
-     this.vel += this.acc;
-     this.pos.add(this.core);
-     this.lifespan -= 10;
-  }
+    constructor(x, y) {
+      this.pos = createVector(x, y);
+      this.ell = 0.0;
+      this.vel = 20; 
+      this.acc = 0.2;
+      this.core = createVector(random(-2, 2),random(-2, 2));
+      this.color = color(255, random(155, 200), random(155, 200) );
+    }
     
-  display() {
-    push();
-    translate(this.pos.x, this.pos.y);
-    rotate(this.ell);
-    fill(127, this.lifespan);
-    ellipse(0, 0, 3);
-    pop();
-  }
-
-  isDead() {
-    return this.lifespan < 0;
-  } 
+       addForce(aForce) {
+       this.core.add(aForce);
+     }
+    
+    update() {
+       this.ell += this.vel;
+       this.vel += this.acc;
+       this.pos.add(this.core);
+    }
+      
+    display() {
+      push();
+      translate(this.pos.x, this.pos.y);
+      rotate(this.ell);
+      stroke(this.color);
+      fill(this.color);
+      ellipse(0, 0, 10);
+      pop();
+    }
   
-}
+    isDead() {
+      return this.lifespan < 0;
+    } 
+    
+  }
+  

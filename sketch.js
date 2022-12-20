a9a595354a21640a3f470e74bf5e8511d3f9242b
yena0910 <yenayena1217@naver.com>
2022-12-20 19:32:25 +0900

    파일 정리

--- sketch.js
+++ sketch.js
@@ -1,7 +1,31 @@
+let ball;
+let particle = [];
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
+
+  for(let i = 0; i < 10; i++) {
+  particle.push(new Mover(random(0, width),random(0, height), 3))
+  }
 }
 
 function draw() {
-  background(220);
+  background(0);
+
+  for (let ball of particle) {
+    ball.update();
+    ball.show();
+    ball.edge();
+    ball.branch();
+  }
+
+
 }
+
+
+
+
+
+
+
+

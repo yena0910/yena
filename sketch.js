4151e95dd7c6bb3b49d9535d8ad8703fb391b17b
yena0910 <yenayena1217@naver.com>
2022-12-20 19:56:22 +0900

    작품설명 추가

--- sketch.js
+++ sketch.js
@@ -1,8 +1,10 @@
 let ball;
 let particle = [];
+let work;
 
 function setup() {
   createCanvas(800, 800);
+  work = createP('2223038 최예나              작품명:기쁨             설명:제작자의 행복했을 때의 내면을 표현해보았다.');
 
   for(let i = 0; i < 10; i++) {
   particle.push(new Mover(random(0, width),random(0, height), 3))
